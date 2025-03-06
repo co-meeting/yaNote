d769116d500b7eaeda17bc3a441dbcd1ea284962
yanotaka <takaaki.yano@co-meeting.com>
2025-03-07 01:45:41 +0900

    v1.3（2025-03-07）

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "yaNote-cache-v1.2.13.3";
+const CACHE_NAME = "yaNote-cache-v1.3";
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png"
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192x192.png",
+  "./icons/icon-512x512.png"
 ];
 
 self.addEventListener("install", (event) => {
