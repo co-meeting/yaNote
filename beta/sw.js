20a158decf33238435d1f8807d59af90aa5e11cd
yanotaka <takaaki.yano@co-meeting.com>
2025-03-20 15:18:11 +0900

    v1.4-beta.4（2025-03-20）

--- beta/sw.js
+++ beta/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "yaNote-beta-cache-v1.4-beta.3"; // バージョン番号を最新に更新
+const CACHE_NAME = "yaNote-beta-cache-v1.4-beta.4"; // バージョン番号を最新に更新
 const APP_ASSETS = [
   "./",
   "./index.html",
